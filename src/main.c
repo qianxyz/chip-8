23d44639aad3f1abd2ab8069fa86bf1685fd27d3
Qianxy <xyqian67@gmail.com>
2022-07-26 22:53:20 +0800

    Add todo roadmap

--- src/main.c
+++ src/main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
-#include "core.h"  // run_emulator
+#include "core.h"
 
+/* TODO: config support
+ * 1. path/to/rom
+ * 2. cpu frequency
+ * 3. modern (CHIP-48/SCHIP) or original (COSMAC VIP)
+ * 4. verbosity
+ */
 int main(int argc, char *argv[])
 {
 	if (argc != 2) {
